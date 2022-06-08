e54b4355fc8ab4a755b39440d52ddb7ff02582ea
Kevin_was_here <xuyang4@ualberta.ca>
2022-06-08 17:07:33 -0600

    added timer and fixed a display issue through terminal

--- program.py
+++ program.py
@@ -1,12 +1,24 @@
-import os
-
+import time
+import subprocess
 
 def closeFile(filename):
     try:
-        os.system('TASKKILL /F /IM '+ filename)
+        subprocess.run('TASKKILL /F /IM '+ filename, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
 
     except:
         print("oops")
 
+def timeToWork(t):
 
-closeFile("firefox.exe")
+    while t:
+        mins, secs = divmod(t, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print("Time left: " + timer, end = "\r")
+        closeFile("chrome.exe")
+        time.sleep(1)
+        t -= 1
+    
+    print("Time is up congrats")
+
+t = input("Enter how many seconds you want to work for: ")
+timeToWork(int(t))
