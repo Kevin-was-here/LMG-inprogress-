0709222513fd959be627d1d50709feea8863612c
Kevin_was_here <xuyang4@ualberta.ca>
2022-06-08 17:55:50 -0600

    tested for timings and stuff

--- program.py
+++ program.py
@@ -1,24 +1,50 @@
 import time
 import subprocess
 
-def closeFile(filename):
-    try:
-        subprocess.run('TASKKILL /F /IM '+ filename, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+"""
+Function takes a executable name and forcefully murders it
 
+Pre: Filename
+post: none
+"""
+def executeprog(progname):
+    try:
+        subprocess.run('TASKKILL /F /IM '+ progname, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
     except:
         print("oops")
 
+"""
+Timer function takes the seconds and runs a countdown timer, displays the number of seconds remaining
+
+pre: timer in seconds
+post: none
+"""
 def timeToWork(t):
 
     while t:
         mins, secs = divmod(t, 60)
         timer = '{:02d}:{:02d}'.format(mins, secs)
         print("Time left: " + timer, end = "\r")
-        closeFile("chrome.exe")
-        time.sleep(1)
+        execute()
+        time.sleep(0.5)
         t -= 1
     
     print("Time is up congrats")
+
+"""
+Kill function checks for if a program is open and sends the program name to be executed
+pre: none
+post: none
+"""
+def execute():
+    progList = ["Minecraft.exe","RiotClientUx.exe",
+                "BootstrapPackagedGame","VALORANT-Win64-Shipping.exe"]
+
+    for i in progList:
+        executeprog(i)
+    
+    
+
 
 t = input("Enter how many seconds you want to work for: ")
 timeToWork(int(t))
